a4f4358df50972593ad4832df4a07d811af18edf
kadirzkaya <bmkozkaya@gmail.com>
2022-09-20 14:52:53 +0300

    Harf yazdırma

--- src/HarfYazdirma.java
+++ src/HarfYazdirma.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class HarfYazdirma {
+public class HarfYazdirma {
+    public static void main(String[] args) {
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row:letter){
+            for (String col:row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
 }
